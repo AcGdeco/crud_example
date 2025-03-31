95d75a9217406b3d11127e37857cfa029c2df266
AcGdeco <andrecristiangoncalves@hotmail.com>
2025-03-31 13:53:28 -0300

    :tada: Initial Commit

---
+++ Block/Adminhtml/Index/Edit/Button/Delete.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright © Emipro Technologies Pvt Ltd. All rights reserved.
+ * @license http://shop.emiprotechnologies.com/license-agreement/
+ */
+
+namespace Emipro\Custom\Block\Adminhtml\Index\Edit\Button;
+
+use Magento\Backend\Block\Widget\Context;
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+
+class Delete extends Generic implements ButtonProviderInterface
+{
+    /**
+     * @var Context
+     */
+    protected $context;
+
+    /**
+     * @param Context $context
+     * @param AuthorRepositoryInterface $authorRepository
+     */
+
+    public function __construct(
+        Context $context
+    ) {
+        $this->context = $context;
+    }
+
+    /**
+     * get button data
+     *
+     * @return array
+     */
+    public function getButtonData()
+    {
+        $data = [];
+        $cep_range_id = $this->context->getRequest()->getParam('cep_range_id');
+        if ($cep_range_id) {
+            $data = [
+                'label' => __('Delete'),
+                'class' => 'delete',
+                'on_click' => 'deleteConfirm(\'' . __(
+                    'Are you sure you want to do this?'
+                ) . '\', \'' . $this->getDeleteUrl() . '\')',
+                'sort_order' => 20,
+            ];
+        }
+        return $data;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeleteUrl()
+    {
+        $cep_range_id = $this->context->getRequest()->getParam('cep_range_id');
+        return $this->getUrl('*/*/delete', ['cep_range_id' => $cep_range_id]);
+    }
+}
---
+++ Controller/Adminhtml/Index/Save.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Copyright © Emipro Technologies Pvt Ltd. All rights reserved.
+ * @license http://shop.emiprotechnologies.com/license-agreement/
+ */
+namespace Emipro\Custom\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Emipro\Custom\Model\Job;
+use Magento\Backend\Model\Session;
+
+class Save extends \Magento\Backend\App\Action
+{
+    protected $Custommodel;
+    protected $adminsession;
+
+    public function __construct(
+        Action\Context $context,
+        Job $Custommodel,
+        Session $adminsession
+    ) {
+        parent::__construct($context);
+        $this->Custommodel = $Custommodel;
+        $this->adminsession = $adminsession;
+    }
+
+    public function execute()
+    {
+        $data = $this->getRequest()->getPostValue();
+
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($data) {
+            $cep_range_id = $this->getRequest()->getParam('cep_range_id');
+
+            if ($cep_range_id) {
+                $this->Custommodel->load($cep_range_id);
+            }
+
+            $this->Custommodel->setData($data);
+
+            try {
+                $this->Custommodel->save();
+                $this->messageManager->addSuccess(__('The data has been saved.'));
+                $this->adminsession->setFormData(false);
+                if ($this->getRequest()->getParam('back')) {
+                    if ($this->getRequest()->getParam('back') == 'add') {
+                        return $resultRedirect->setPath('*/*/add');
+                    } else {
+                        $asdf = $resultRedirect->setPath(
+                            '*/*/edit',
+                            [
+                                'cep_range_id' => $this->Custommodel->getCepRangeId(),
+                                '_current' => true
+                            ]
+                        );
+                        return $resultRedirect->setPath(
+                            '*/*/edit',
+                            [
+                                'cep_range_id' => $this->Custommodel->getCepRangeId(),
+                                '_current' => true
+                            ]
+                        );
+                    }
+                }
+                return $resultRedirect->setPath('*/*/');
+            } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\RuntimeException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\Exception $e) {
+                $this->messageManager->addException($e, __('Something went wrong while saving the data.'));
+            }
+
+            $this->_getSession()->setFormData($data);
+            return $resultRedirect->setPath('*/*/edit', ['cep_range_id' => $this->getRequest()->getParam('cep_range_id')]);
+        }
+        return $resultRedirect->setPath('*/*/');
+    }
+}
